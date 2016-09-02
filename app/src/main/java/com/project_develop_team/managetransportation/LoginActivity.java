2240e06b71f04aa3f4fd22fbc4582dbbac2ac477
thanapon <patpat_501@hotmail.com>
2016-09-02 14:09:55 +0700

    First SplashScreen

---
+++ app/src/main/java/com/project_develop_team/managetransportation/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.project_develop_team.managetransportation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.usernameEditText)
+    EditText usernameEditText;
+    @BindView(R.id.passwordEditText)
+    EditText passwordEditText;
+
+    @BindView(R.id.loginButton)
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
---
+++ app/src/main/java/com/project_develop_team/managetransportation/SplashScreen.java
@@ -0,0 +1,51 @@
+package com.project_develop_team.managetransportation;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Handler handler;
+
+    Runnable runnable;
+
+    long delayTime;
+
+    long time = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        handler = new Handler();
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
+        time = delayTime - (System.currentTimeMillis() - time);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        delayTime = time;
+        handler.postDelayed(runnable, delayTime);
+        time = System.currentTimeMillis();
+    }
+}
