45ab7027e648d2a2a199ea10f30a0c68b2df1194
thanapon <patpat_501@hotmail.com>
2016-09-19 18:22:35 +0700

    Doing ListView (unready)

--- app/src/main/java/com/project_develop_team/managetransportation/TaskListFragment.java
+++ app/src/main/java/com/project_develop_team/managetransportation/TaskListFragment.java
@@ -1,47 +1,20 @@
 package com.project_develop_team.managetransportation;
 
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import com.google.firebase.database.DatabaseReference;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import com.google.firebase.database.Query;
 
 
-public class TaskListFragment extends Fragment {
-
-    @BindView(R.id.datesTaskTextView)
-    TextView datesTaskTextView;
+public class TaskListFragment extends ListFragment {
 
     public TaskListFragment() {
-
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_task_list, container, false);
-        ButterKnife.bind(this, view);
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE d MMMM", Locale.getDefault());
-        datesTaskTextView.setText(simpleDateFormat.format(new Date()));
-
-        return view;
+    public Query getQuery(DatabaseReference databaseReference) {
+        return databaseReference;
     }
 
 }
