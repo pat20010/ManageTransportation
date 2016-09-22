4f80eb3bd1e0d93f5eea359c3e12bcf9b1b0b2a1
thanapon <patpat_501@hotmail.com>
2016-09-22 16:50:00 +0700

    Add retrieve data from Firebase

--- app/src/main/java/com/project_develop_team/managetransportation/SaveListActivity.java
+++ app/src/main/java/com/project_develop_team/managetransportation/SaveListActivity.java
@@ -2,12 +2,83 @@ package com.project_develop_team.managetransportation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.project_develop_team.managetransportation.models.Tasks;
+
+import butterknife.BindView;
 
 public class SaveListActivity extends AppCompatActivity {
+
+    private DatabaseReference databaseReference;
+
+    @BindView(R.id.nameTextView)
+    TextView nameTextView;
+    @BindView(R.id.taskNameTextView)
+    TextView taskNameTextView;
+    @BindView(R.id.taskAddressTextView)
+    TextView taskAddressTextView;
+    @BindView(R.id.taskPhoneTextView)
+    TextView taskPhoneTextView;
+
+    private ValueEventListener mEventListener;
+
+    String tasksKey;
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_list);
+
+        tasksKey = getIntent().getStringExtra("tasks-key");
+        if (tasksKey == null) {
+            throw new IllegalArgumentException("Must pass tasks-key");
+        }
+        databaseReference = FirebaseDatabase.getInstance().getReference()
+                .child("tasks").child(tasksKey);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Tasks tasks = dataSnapshot.getValue(Tasks.class);
+                nameTextView.setText(tasks.name);
+                taskNameTextView.setText(tasks.taskName);
+                taskAddressTextView.setText(tasks.taskAddress);
+                taskPhoneTextView.setText(tasks.taskPhone);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), "Fail load task", Toast.LENGTH_SHORT).show();
+            }
+        };
+        databaseReference.addValueEventListener(eventListener);
+
+        mEventListener = eventListener;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mEventListener != null) {
+            databaseReference.removeEventListener(mEventListener);
+        }
     }
 }
--- app/src/main/java/com/project_develop_team/managetransportation/models/Tasks.java
+++ app/src/main/java/com/project_develop_team/managetransportation/models/Tasks.java
@@ -4,6 +4,7 @@ package com.project_develop_team.managetransportation.models;
 public class Tasks {
 
     public String uid;
+    public String name;
     public String taskName;
     public String taskAddress;
     public String taskPhone;
@@ -11,10 +12,12 @@ public class Tasks {
     public Tasks() {
     }
 
-    public Tasks(String uid, String taskName, String taskAddress, String taskPhone) {
+    public Tasks(String uid, String name, String taskName, String taskAddress, String taskPhone) {
         this.uid = uid;
+        this.name = name;
         this.taskName = taskName;
         this.taskAddress = taskAddress;
         this.taskPhone = taskPhone;
     }
+
 }
