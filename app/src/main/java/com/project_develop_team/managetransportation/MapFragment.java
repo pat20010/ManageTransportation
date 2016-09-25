843a20163535dd336be7fdc8afeb9fdcfa29fa76
thanapon <patpat_501@hotmail.com>
2016-09-25 16:19:36 +0700

    Add MyLocation and checkPermissionLocation

--- app/src/main/java/com/project_develop_team/managetransportation/MapFragment.java
+++ app/src/main/java/com/project_develop_team/managetransportation/MapFragment.java
@@ -1,14 +1,31 @@
 package com.project_develop_team.managetransportation;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment {
+
+public class MapFragment extends Fragment implements OnMapReadyCallback, LocationListener {
+
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+
+    private GoogleMap mMap;
 
     public MapFragment() {
 
@@ -24,7 +41,37 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return view;
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng dpu = new LatLng(13.8703061, 100.5485473);
+        mMap.addMarker(new MarkerOptions().position(dpu).title("DPU"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(dpu));
+        checkMyLocationPermission();
+    }
+
+    public void checkMyLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
 }
